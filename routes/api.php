a36da54a4891cd28f035478b08f87a372ada1808
Shafnas <mshafnas125@gmail.com>
2020-08-29 11:34:16 +0530

    crud functions

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::apiResources(['product' => 'API\ProductController']);
+Route::get('product', 'API\ProductController@index');
+Route::get('product/{id}', 'API\ProductController@show');
+Route::post('product', 'API\ProductController@store');
+Route::put('product/{id}', 'API\ProductController@update');
+Route::delete('product/{id}', 'API\ProductController@destroy');
